eb9b492ec960d0fc4e9928c186e3f9d2388edb39
Bilha <bilha.leposo@student.moringaschool.com>
2024-03-30 16:13:24 +0300

    complete array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(item =>item.result === "W");
+    return win ? win.year : undefined;
+}
+console.log(superbowlWin(record));  // Output: 2015
+
+const sadReality = [
+    //Records with no win objects
+];
+
+console.log(superbowlWin(sadReality));  //Output: undefined
